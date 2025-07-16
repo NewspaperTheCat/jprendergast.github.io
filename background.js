b08823e4c42352cc7ea71376df508ef2777e15d8
NewspaperTheCat <jmprendergast@wpi.edu>
2025-07-16 18:27:10 -0500

    Parallax blur and borders

--- background.js
+++ background.js
@@ -4,11 +4,9 @@ palette["vanilla"] = "#F3E5AB";
 // palette["flax"] = "#E3D985";
 palette["crayola"] = "#E57A44";
 palette["claret"] = "#740F35";
-console.log(palette);
 
-const numElements = 25;
+const numElements = 50;
 const horizontalMargin = 50;
-const pageHeight = 1000;
 
 var parallaxParent;
 var parallaxElements;
@@ -16,32 +14,46 @@ var parallaxInitials;
 var parallaxDepths;
 
 addEventListener("load", (event) => {
-    console.log("hello")
     // recollect on load
     parallaxParent = document.getElementsByClassName("parallax")[0];
     parallaxElements = [];
     parallaxInitials = [];
     parallaxDepths = [];
 
+    let pageHeight = document.documentElement.scrollHeight
+
+    // make depths coherent with overlap
+    for (let i = 0; i < numElements; i++) {
+        parallaxDepths.push(1 - Math.random() * .9);
+    }
+    parallaxDepths.sort();
+
     // populate parallax elements
     for (let i = 0; i < numElements; i++) {
         let y = pageHeight * Math.random();
         parallaxInitials.push(y);
 
-        parallaxDepths.push(1 - Math.random() * .9);
+        let x = (visualViewport.width - horizontalMargin * 2) * Math.random() + horizontalMargin;
+        let from = "left:";
+        if (x > visualViewport.width / 2) {
+            x = visualViewport.width - x;
+            from = "right:";
+        }
 
-        let x = 100 * Math.random();
-
-        let rot = Math.random() * 360;
+        // let rot = Math.random() * 360;
+        let rot = 0;
 
         let keys = Object.keys(palette);
         let color = palette[keys[Math.floor(Math.random() * keys.length)]];
 
-        parallaxParent.insertAdjacentHTML("beforeend", "<div class='square' style='left:" + x + "%; top:" + y + "px; background-color:" + color + "; rotate:" + rot + "deg;'></div>");
-    }
+        let blur = 4 * (Math.pow(1 - parallaxDepths[i], 2));
 
-    // make depths coherent with overlap
-    parallaxDepths.sort();
+        parallaxParent.insertAdjacentHTML("beforeend", "<div class='square' style='"
+            + from + x + "px; top:" + y
+            + "px; background-color:" + color
+            + "; rotate:" + rot + "deg; filter: blur(" + blur
+            + "px);'></div>");
+    }
 
     // gather parallax elements
     parallaxElements = parallaxParent.children;
